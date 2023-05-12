fbde29e8c719415d9b5ce5b63f029fc7a7a878e7
Bota509 <alinut509@gmail.com>
2023-05-12 15:23:50 +0300

    Product View

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import View.ClientView;
+import View.ProductView;
 
 public class Main {
     public static void main(String[] args) {
 
         ClientView clientView = new ClientView();
+        ProductView productView = new ProductView();
     }
 }
--- src/View/ProductView.java
+++ src/View/ProductView.java
@@ -10,11 +10,11 @@ public class ProductView extends JFrame {
     private JPanel contentPane;
     private JTextField idTextField;
     private JTextField nameTextField;
-    private JTextField adressTextField;
-    private JTextField emailTextField;
-    private JTextField ageTextField;
+    private JTextField priceTextField;
+    private JTextField categoryTextField;
+    private JTextField descriptionTextField;
     private JTable table;
-
+    
     public  ProductView()
     {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -26,22 +26,22 @@ public class ProductView extends JFrame {
         setContentPane(contentPane);
         contentPane.setLayout(null);
 
-        JLabel labelClient = new JLabel("Clients");
+        JLabel labelClient = new JLabel("Products");
         labelClient.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 26));
         labelClient.setBounds(533, 33, 133, 38);
         contentPane.add(labelClient);
 
-        JButton addNewClientButton = new JButton("Add New Client");
+        JButton addNewClientButton = new JButton("Add New Product");
         addNewClientButton.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 26));
         addNewClientButton.setBounds(158, 85, 314, 50);
         contentPane.add(addNewClientButton);
 
-        JButton editClientButton = new JButton("Edit Client");
+        JButton editClientButton = new JButton("Edit Product");
         editClientButton.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 26));
         editClientButton.setBounds(158, 171, 314, 50);
         contentPane.add(editClientButton);
 
-        JButton deleteClient = new JButton("Delete Client");
+        JButton deleteClient = new JButton("Delete Product");
         deleteClient.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 26));
         deleteClient.setBounds(158, 247, 314, 50);
         contentPane.add(deleteClient);
@@ -51,7 +51,7 @@ public class ProductView extends JFrame {
         contentPane.add(idTextField);
         idTextField.setColumns(10);
 
-        JButton showClientsButton = new JButton("Show Clients");
+        JButton showClientsButton = new JButton("Show Products");
         showClientsButton.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 26));
         showClientsButton.setBounds(158, 333, 314, 50);
         contentPane.add(showClientsButton);
@@ -61,20 +61,20 @@ public class ProductView extends JFrame {
         nameTextField.setBounds(288, 470, 184, 43);
         contentPane.add(nameTextField);
 
-        adressTextField = new JTextField();
-        adressTextField.setColumns(10);
-        adressTextField.setBounds(288, 538, 184, 43);
-        contentPane.add(adressTextField);
+        priceTextField = new JTextField();
+        priceTextField.setColumns(10);
+        priceTextField.setBounds(288, 538, 184, 43);
+        contentPane.add(priceTextField);
 
-        emailTextField = new JTextField();
-        emailTextField.setColumns(10);
-        emailTextField.setBounds(288, 602, 184, 43);
-        contentPane.add(emailTextField);
+        categoryTextField = new JTextField();
+        categoryTextField.setColumns(10);
+        categoryTextField.setBounds(288, 602, 184, 43);
+        contentPane.add(categoryTextField);
 
-        ageTextField = new JTextField();
-        ageTextField.setColumns(10);
-        ageTextField.setBounds(288, 671, 184, 43);
-        contentPane.add(ageTextField);
+        descriptionTextField = new JTextField();
+        descriptionTextField.setColumns(10);
+        descriptionTextField.setBounds(288, 671, 184, 43);
+        contentPane.add(descriptionTextField);
 
         JLabel idLabel = new JLabel("Id");
         idLabel.setFont(new Font("Tahoma", Font.PLAIN, 22));
@@ -86,20 +86,20 @@ public class ProductView extends JFrame {
         nameLabel.setBounds(158, 470, 120, 43);
         contentPane.add(nameLabel);
 
-        JLabel adressLabel = new JLabel("Adress");
-        adressLabel.setFont(new Font("Tahoma", Font.PLAIN, 22));
-        adressLabel.setBounds(158, 538, 120, 43);
-        contentPane.add(adressLabel);
+        JLabel priceLabel = new JLabel("Price");
+        priceLabel.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        priceLabel.setBounds(158, 538, 120, 43);
+        contentPane.add(priceLabel);
 
-        JLabel emailLabel = new JLabel("Email");
-        emailLabel.setFont(new Font("Tahoma", Font.PLAIN, 22));
-        emailLabel.setBounds(158, 602, 120, 43);
-        contentPane.add(emailLabel);
+        JLabel categoryLabel = new JLabel("Category");
+        categoryLabel.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        categoryLabel.setBounds(158, 602, 120, 43);
+        contentPane.add(categoryLabel);
 
-        JLabel ageLabel = new JLabel("Age");
-        ageLabel.setFont(new Font("Tahoma", Font.PLAIN, 22));
-        ageLabel.setBounds(158, 671, 120, 43);
-        contentPane.add(ageLabel);
+        JLabel descriptionLabel = new JLabel("Description");
+        descriptionLabel.setFont(new Font("Tahoma", Font.PLAIN, 22));
+        descriptionLabel.setBounds(158, 671, 120, 43);
+        contentPane.add(descriptionLabel);
 
         JScrollPane clientsTable = new JScrollPane();
         clientsTable.setBounds(665, 81, 643, 633);
